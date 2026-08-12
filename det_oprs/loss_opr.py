561c532f82525894f49e775fac33ac58f17098e0
Thanhmay2406 <Thanhmay2406@gmail.com>
2026-08-12 17:02:10 +0700

    Remove extra tracked files

--- det_oprs/loss_opr.py
+++
@@ -1,9 +0,0 @@
-import torch.nn.functional as F
-
-
-def softmax_loss(logits, labels):
-    return F.cross_entropy(logits, labels.long(), reduction="none")
-
-
-def smooth_l1_loss(pred, target, beta):
-    return F.smooth_l1_loss(pred, target, beta=beta, reduction="none").sum(dim=1)
